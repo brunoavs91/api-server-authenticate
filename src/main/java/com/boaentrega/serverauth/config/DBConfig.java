17368718ff273e614c3793fa9818bbe5a3bbe7f7
brunoav91@gmail.com <brunoav91@gmail.com>
2021-08-29 16:55:36 -0300

    new user

--- src/main/java/com/boaentrega/serverauth/config/DBConfig.java
+++ src/main/java/com/boaentrega/serverauth/config/DBConfig.java
@@ -7,11 +7,13 @@ import com.boaentrega.serverauth.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Arrays;
 
 @Configuration
+@Profile("prod")
 public class DBConfig {
 
     @Autowired
