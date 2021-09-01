10d71bc2374518f507a9bd2cb2a6cccc86ff6623
brunoav91@gmail.com <brunoav91@gmail.com>
2021-08-31 22:04:04 -0300

    fix bug

--- src/main/java/com/boaentrega/serverauth/ServerAuthApplication.java
+++ src/main/java/com/boaentrega/serverauth/ServerAuthApplication.java
@@ -3,6 +3,7 @@ package com.boaentrega.serverauth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
